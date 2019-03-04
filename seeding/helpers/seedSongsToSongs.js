1526b7de5351e3931fc89fa8849572d4235cd73b
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-03-04 12:10:20 -0800

    Complete neo4j seed query

---
+++ seeding/helpers/seedSongsToSongs.js
@@ -0,0 +1,38 @@
+const writeToFile = require('./promiseWriteToFile');
+
+module.exports = (filePath, OoM) => new Promise(async (resolve, reject) => {
+  try {
+    const songToSong = {
+      songId: 1,
+      relatedSongId: 1,
+    };
+    for (let i = 0; i < OoM * 1000; i += 1) {
+      let batch = [];
+      if (i === 0) {
+        batch.push(`${Object.keys(songToSong).join(',')}`);
+      }
+      for (let b = 0; b < 10; b += 1) {
+        if (songToSong.songId === songToSong.relatedSongId) {
+          if (songToSong.relatedSongId >= (OoM * 1000)) {
+            songToSong.relatedSongId = 1;
+          } else {
+            songToSong.relatedSongId += 1;
+          }
+        }
+        batch.push([
+          `${songToSong.songId}`,
+          `${songToSong.relatedSongId}`,
+        ].join(','));
+        if (songToSong.relatedSongId >= (OoM * 1000)) songToSong.relatedSongId = 1;
+        songToSong.relatedSongId += 1;
+      }
+      songToSong.songId += 1;
+      // eslint-disable-next-line no-await-in-loop
+      await writeToFile(filePath, batch, i);
+      batch = [];
+    }
+    resolve();
+  } catch (error) {
+    reject(error);
+  }
+});
