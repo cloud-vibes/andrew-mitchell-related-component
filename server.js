45e5f74f94f2a1df992bcf3f79ecd684a1e0ab14
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2019-03-07 18:49:34 -0800

    Add artillery stress testing

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
 const { makeAugmentedSchema } = require('neo4j-graphql-js');
 const neo4j = require('neo4j-driver').v1;
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
 const typeDefs = require('./app/models/graphql');
 const { neo4jPass, neo4jUser } = require('./config');
 
-const schema = makeAugmentedSchema({ typeDefs });
+const schema = makeAugmentedSchema({
+  typeDefs,
+  config: {
+    debug: false,
+  },
+});
 
 const driver = neo4j.driver(
   'bolt://localhost:7687',
@@ -27,7 +29,7 @@ app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
-})
+});
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
